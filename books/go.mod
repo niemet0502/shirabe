1638475e63e813b5afd9229e9c9425cc656192c9
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-07-16 21:26:26 +0000

    init the books service

---
+++ books/go.mod
@@ -0,0 +1,13 @@
+module github.com/niemet0502/shirabe/books
+
+go 1.22.0
+
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240711142825-46eb208f015d // indirect
+	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
---
+++ books/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/niemet0502/shirabe/books/database"
+	"github.com/niemet0502/shirabe/books/repository"
+	"github.com/niemet0502/shirabe/books/service"
+
+	pb "github.com/niemet0502/shirabe/books/book"
+	"github.com/niemet0502/shirabe/books/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	fmt.Println("Books service")
+
+	// load env variable
+	db := database.InitDb()
+
+	defer db.Close()
+
+	// create the repository
+
+	repo := repository.NewBookRepository(db)
+
+	// create the service
+	svc := service.NewBookService(repo)
+
+	// create the handler
+
+	// create routes
+
+	// create the server and start listening
+
+	gs := grpc.NewServer()
+
+	c := server.NewBook(svc)
+
+	pb.RegisterBookServer(gs, c)
+
+	reflection.Register(gs)
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", 9002))
+
+	if err != nil {
+		fmt.Print("Unable to create listener")
+	}
+
+	gs.Serve(l)
+}
