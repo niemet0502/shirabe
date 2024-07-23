a4fa82ed96d72ff8ab1dadcedc9f7abfacf78958
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-07-23 09:35:02 +0000

    UserService :: init the project

---
+++ users/go.mod
@@ -0,0 +1,13 @@
+module github.com/niemet0502/shirabe/users
+
+go 1.22.0
+
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+)
--- users/main.go
+++ users/main.go
@@ -1,1 +1,40 @@
-package users
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/niemet0502/shirabe/users/database"
+	"github.com/niemet0502/shirabe/users/repository"
+	"github.com/niemet0502/shirabe/users/server"
+	"github.com/niemet0502/shirabe/users/service"
+	pb "github.com/niemet0502/shirabe/users/user"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db := database.InitDb()
+
+	repo := repository.NewUserRepository(db)
+
+	svc := service.NewUserService(repo)
+
+	gs := grpc.NewServer()
+
+	c := server.NewServer(svc)
+
+	pb.RegisterUserServiceServer(gs, c)
+
+	reflection.Register(gs)
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", 9004))
+
+	if err != nil {
+		fmt.Print("Unable to create listener")
+	}
+
+	gs.Serve(l)
+
+}
