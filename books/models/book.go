889b70c21db384dbf74caaf49f541ea2be5d704c
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-07-25 14:26:44 +0000

    BookService :: regenerated pb files

--- books/models/book.go
+++ books/models/book.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -12,13 +14,21 @@ type Book struct {
 	Status          int
 	TotalPages      int
 	ReadingProgress int
-	UserId          int
+	UserId          int `gorm:"not null"`
+	Cover           string
+	Description     string
+	PublicatedAt    time.Time `gorm:"default:null"`
+	StartReadingAt  time.Time `gorm:"default:null"`
+	FinishReadingAt time.Time `gorm:"default:null"`
 }
 
 type CreateBook struct {
-	Title      string
-	Author     string
-	Genre      string
-	TotalPages int
-	UserId     int
+	Title        string
+	Author       string
+	Genre        string
+	TotalPages   int
+	UserId       int
+	Cover        string
+	Description  string
+	PublicatedAt time.Time
 }
