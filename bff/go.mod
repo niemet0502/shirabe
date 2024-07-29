f38980c63f3aab3211e4265a2ef60f13b8f22736
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-07-29 11:41:56 +0000

    Bff :: init the project

---
+++ bff/go.mod
@@ -0,0 +1,15 @@
+module github.com/niemet0502/shirabe/bff
+
+go 1.22.0
+
+require github.com/niemet0502/shirabe/books v0.0.0-20240727234600-74ef196d99a4
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240711142825-46eb208f015d // indirect
+	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
--- bff/main.go
+++ bff/main.go
@@ -1,1 +1,51 @@
-package gateway
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/niemet0502/shirabe/bff/handlers"
+	"github.com/niemet0502/shirabe/bff/services"
+	bookProto "github.com/niemet0502/shirabe/books/book"
+
+	"github.com/gorilla/mux"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	PORT = ":9005"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+	// create client for each grpc
+	conn, err := grpc.NewClient("localhost:9002", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	bookClient := bookProto.NewBookClient(conn)
+
+	// create each service
+	bookSvc := services.NewBookService(bookClient)
+
+	// create handlers
+	bookHandler := handlers.NewBookHandler(bookSvc)
+
+	// create routes
+	r.HandleFunc("/books/{id:[0-9]+}", bookHandler.GetBook)
+	r.HandleFunc("/books", bookHandler.GetBooks)
+	r.HandleFunc("/books/search", bookHandler.SearchBooks)
+	r.HandleFunc("/books", bookHandler.CreateBook).Methods("POST")
+
+	// start the server
+	err = http.ListenAndServe(PORT, r)
+
+	if err != nil {
+		log.Fatal("The server didn't start")
+	}
+}
